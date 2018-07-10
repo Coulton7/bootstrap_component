e57f7c4c30a4303dbcb0124c863643a57e17b026
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-07-10 11:06:18 +0100

    First Initial Files upload

---
+++ template.php
@@ -0,0 +1,32 @@
+<?php
+
+function bootstrap_component_preprocess_page(&$variables) {
+    drupal_add_library('system', 'ui.slider');
+  if (isset($variables['node']->type)) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+}
+
+
+
+
+function bootstrap_component_preprocess_node(&$variables) {
+
+  // Get a list of all the regions for this theme
+  foreach (system_region_list($GLOBALS['theme']) as $region_key => $region_name) {
+
+    // Get the content for each region and add it to the $region variable
+    if ($blocks = block_get_blocks_by_region($region_key)) {
+      $variables['region'][$region_key] = $blocks;
+    }
+    else {
+      $variables['region'][$region_key] = array();
+    }
+  }
+}
+/**
+ * @file
+ * template.php
+ */
