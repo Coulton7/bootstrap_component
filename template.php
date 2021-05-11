e959e9cc5c372bced08689e4d2ca093f6112d347
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-11 13:21:41 +0100

    Updating to turn to no cdn theme

--- template.php
+++ template.php
@@ -9,7 +9,19 @@ function bootstrap_component_preprocess_page(&$variables) {
   }
 }
 
-
+function bootstrap_component_css_alter(&$css){
+  $css['sites/all/themes/f4s_bs/bootstrap/css/bootstrap.min.css']['weight']=11;
+  $css['sites/all/themes/f4s_bs/bootstrap/css/bootstrap-theme.min.css']['weight']=12;
+  $css['sites/all/themes/aesbs337/css/fonts-style.css']['weight']=13;
+  $css['sites/all/themes/aesbs337/css/regions-style.css']['weight']=14;
+  $css['sites/all/themes/aesbs337/css/block-style.css']['weight']=15;
+  $css['sites/all/themes/aesbs337/css/field-style.css']['weight']=16;
+  $css['sites/all/themes/aesbs337/css/jquery.scrolling-tabs.min.css']['weight']=17;
+  $css['sites/all/themes/f4s_bs/css/fonts-style.css']['weight']=18;
+  $css['sites/all/themes/f4s_bs/css/regions-style.css']['weight']=19;
+  $css['sites/all/themes/f4s_bs/css/block-style.css']['weight']=20;
+  $css['sites/all/themes/f4s_bs/css/field-style.css']['weight']=21;
+}
 
 
 function bootstrap_component_preprocess_node(&$variables) {
